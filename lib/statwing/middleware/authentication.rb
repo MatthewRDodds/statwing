5eb40e436ec14477b5872dff587b47e0a7c0b850
Matthew Dodds <matthewrusselldodds@gmail.com>
2015-04-08 13:35:50 -0400

    Update spacing for middleware

--- lib/statwing/middleware/authentication.rb
+++ lib/statwing/middleware/authentication.rb
@@ -1,10 +1,10 @@
 module Statwing
-	class Authentication < Faraday::Middleware
-	  def call(env)
-	    key = Base64.encode64(Statwing.configuration.api_key).gsub!("\n", '')
-	    env[:request_headers]["Authorization"] = "Basic #{key}:"
+  class Authentication < Faraday::Middleware
+    def call(env)
+      key = Base64.encode64(Statwing.configuration.api_key).gsub!("\n", '')
+      env[:request_headers]["Authorization"] = "Basic #{key}:"
 
-	    @app.call(env)
-	  end
-	end
+      @app.call(env)
+    end
+  end
 end
--- lib/statwing/middleware/parser.rb
+++ lib/statwing/middleware/parser.rb
@@ -1,16 +1,16 @@
 module Statwing
-	class Parser < Faraday::Response::Middleware
-	  def on_complete(env)
-	    json = JSON.parse(env[:body], :symbolize_names => true)
-	    error = json.delete(:error) || {}
+  class Parser < Faraday::Response::Middleware
+    def on_complete(env)
+      json = JSON.parse(env[:body], :symbolize_names => true)
+      error = json.delete(:error) || {}
 
-	    # Raise error when request returns an error
-	    if error.present?
-	    	fail "#{error[:type].classify}: #{error[:message]}"
-	    end
+      # Raise error when request returns an error
+      if error.present?
+        fail "#{error[:type].classify}: #{error[:message]}"
+      end
 
-	    metadata = json.delete(:metadata) || []
-	    env[:body] = { :data => json, :errors => error, :metadata => metadata }
-	  end
-	end
+      metadata = json.delete(:metadata) || []
+      env[:body] = { :data => json, :errors => error, :metadata => metadata }
+    end
+  end
 end
